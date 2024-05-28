2f335fcfc0b2b9b04ce9e7be3cf175fec2ba4e07
kelvia.barros <barroskelvia122@gmail.com>
2024-05-28 22:22:04 +0200

    Clase cliente

--- Cliente.java
+++ Cliente.java
@@ -4,14 +4,13 @@ class Cliente {
     private int ITEM_MAX = 15;
     private int ITEM_MIN = 5;
     private int numeroItems;
-
   
     public Cliente() {
     
         this.numeroItems= (int) (Math.random() * (ITEM_MAX - ITEM_MIN + 1)) + ITEM_MIN;
 
     }
-
+    
     public int numeroItems() {
 
         int items=0;
