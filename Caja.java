6a35d2fa1aa9732c08a72c15f91bd3f714050863
xImRaul <144956120+xImRaul@users.noreply.github.com>
2024-05-29 04:09:55 +0200

    clase Caja casi hecha.java

--- Caja.java
+++ Caja.java
@@ -1,19 +1,48 @@
 
 class Caja {
 
+    private int cantidadItems;
+    private int numeroCaja;
+    private int personasAtendidas;
+    private int articulosVendidos;
+
+    public Caja(int numeroCaja){
+        cantidadItems = 0;
+        this.numeroCaja = numeroCaja +1;
+        personasAtendidas = 0;
+        articulosVendidos = 0;
+
+    }
+
     public boolean estaLibre() {
-    
+        return cantidadItems==0;
     }
-
     public void recibe(Cliente cliente) {
-
+        cantidadItems = cliente.entregaItems();
     }
-
     public void atiende() {
 
+        if (cantidadItems>0) {
+            cantidadItems--; 
+            articulosVendidos++;
+            if (cantidadItems==0) {
+                personasAtendidas++; 
+            }
+        }
+
+    }
+    public void verEstado() {
+        System.out.println("Caja " + numeroCaja + ":" + "[" + cantidadItems + "]");
+
     }
 
-    public void verEstado() {
+    public int obtenerPersonasAtendidas(){
 
+        return personasAtendidas;
+    }
+
+    public int obtenerArticulosVendidos(){
+
+        return articulosVendidos;
     }
 }
