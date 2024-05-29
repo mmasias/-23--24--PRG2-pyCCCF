e009e85b4d156c2a70c4faf0549a36696e4a73e6
xImRaul <144956120+xImRaul@users.noreply.github.com>
2024-05-29 12:02:38 +0200

    CentroComercial hecho.java

--- CentroComercial.java
+++ CentroComercial.java
@@ -3,6 +3,7 @@ class CentroComercial {
     private Cliente[] cola;
     private int ultimo;
     private int minutosSinCola;
+    private Estadisticas estadisticas;
 
     public CentroComercial(){
         cajas = new Caja[4];
@@ -12,6 +13,7 @@ class CentroComercial {
         cola = new Cliente[100];
         ultimo = 0;
         minutosSinCola = 0;
+        estadisticas = new Estadisticas();
     }
 
     public void recibe(Cliente cliente) {
@@ -26,23 +28,24 @@ class CentroComercial {
     }
     private void deColaACaja() {
         for(int i=0;i<cajas.length;i++){
-            if (cajas[i].estaLibre()){
+            if (cajas[i].estaLibre() && ultimo>0){
                 Cliente cliente = cola[ultimo-1];
                 ultimo--;
                 cajas[i].recibe(cliente);
-
-            }
-            if (ultimo==0) {
-                minutosSinCola++;
-                break;
+                
             }
         }
     }
     private void atiendeCajas() {
         for (int i = 0; i < cajas.length; i++) {
             cajas[i].atiende();
+            incrementarArticulosVendidos();
+            if (cajas[i].obtenerArticulos() == 0) {
+                incrementarPersonasAtendidas();
+            }
         }
     }
+
     public void verEstado(int minutoActual) {
         System.out.println("Minuto actual: "+minutoActual);
         System.out.println(ultimo + " personas en cola");
@@ -52,16 +55,33 @@ class CentroComercial {
     }
 
     public void mostrarEstadisticasJornada() {
+    public boolean colaVacia() {
+        return ultimo == 0;
+    }
 
         int personasAtendidasTotal = 0;
         for (int i = 0; i < cajas.length; i++) {
             personasAtendidasTotal = personasAtendidasTotal + cajas[i].obtenerPersonasAtendidas();
         }
+    public int obtenerPersonasEnCola() {
+        return ultimo;
+    }
 
         int articulosTotalesVendidos = 0;
         for (int i = 0; i < cajas.length; i++) {
             articulosTotalesVendidos = articulosTotalesVendidos + cajas[i].obtenerArticulosVendidos();
         }
+    public int obtenerPersonasAtendidas() {
+        return estadisticas.obtenerPersonasAtendidas();
+    }
+
+    public int obtenerArticulosVendidos() {
+        return estadisticas.obtenerArticulosVendidos();
+    }
+
+    public void incrementarArticulosVendidos() {
+        estadisticas.incrementarArticulosVendidos();
+    }
 
         System.out.println("RESUMEN");
         System.out.println("=".repeat(60));
@@ -71,6 +91,8 @@ class CentroComercial {
         System.out.println("Articulos vendidos en el dia: " + articulosTotalesVendidos);
         System.out.println("=".repeat(60));
 
+    public void incrementarPersonasAtendidas() {
+        estadisticas.incrementarPersonasAtendidas();
     }
-}
+
 }
