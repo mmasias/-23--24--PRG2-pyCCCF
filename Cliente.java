3a79f2ea71fca1130c840319fe62fc27cd8fc203
Camila Lesly Palma <90413743+Camila-Lesly@users.noreply.github.com>
2024-05-29 17:28:31 +0000

    Añadiendo métodos faltantes en la clase cliente y caja

--- Cliente.java
+++ Cliente.java
@@ -1,4 +1,22 @@
 
 class Cliente {
+    private int packs;
 
+    public Cliente(){
+        this.packs = (int) Math.floor(Math.random()*10+5);
+    }
+
+    public boolean tienePacks(){
+        return packs > 0;
+    }
+
+    public void entregarPack(){
+        if (packs > 0) {
+            packs--; 
+        }
+    }
+
+    public void verEstado(){
+        System.out.println("Número de paquetes " + packs);
+    }
 }
