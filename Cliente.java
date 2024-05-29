8f52c76fee3cea38a3a721ca290d40bb9a34bcb5
xImRaul <144956120+xImRaul@users.noreply.github.com>
2024-05-29 04:03:53 +0200

    Creación y implementación de atributos y métodos en Cliente.java

--- Cliente.java
+++ Cliente.java
@@ -1,4 +1,14 @@
+clase  Cliente {
+    private  int  cantidadItems ;
+     Cliente publico (){
+        cantidadItems = ( int )( Math . random () * 11 )+ 5 ;
+    }
 
-class Cliente {
+    public  int  entregaItems () {
+        int  antiguosItems = cantidadItems ;
+        cantidadArticulos = 0 ;
+        este. cantidadItems = 0;
+        devolver artículos antiguos;
 
+    }
 }
