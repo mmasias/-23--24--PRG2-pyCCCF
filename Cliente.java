9af4336c9bd6086806ef439f66af8e4bc4802507
Maritoz <maritox47@gmail.com>
2024-05-29 19:16:27 +0200

    Clase Cliente

--- Cliente.java
+++ Cliente.java
@@ -1,4 +1,21 @@
-
 class Cliente {
+    private int productos;
 
+    public Cliente() {
+        this.productos = (int) (Math.random() * 5) + 10;
+    }
+
+    public boolean sinProductos() {
+        return productos == 0;
+    }
+
+    public void procesarProducto() {
+        if (productos > 0) {
+            productos--;
+        }
+    }
+
+    public void mostrarEstado() {
+        System.out.print(" / Cliente con " + productos + " productos restantes");
+    }
 }
